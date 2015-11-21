0aa1cfa0c4212410aea8f26c53d981eb6417a347
estduranm <estduranm@gmail.com>
2015-11-20 17:41:01 -0800

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -38,7 +38,7 @@ x_data <- x_data[, mean_and_std_features]
 names(x_data) <- features[mean_and_std_features, 2]
 
 ##########
-# Step 3: Use descriptive activity names to name the activities in the data set
+# Step 3: use activity names to name the activities in the data set
 ##########
 
 #read data
@@ -51,7 +51,7 @@ y_data[, 1] <- activ[y_data[, 1], 2]
 names(y_data) <- "activity"
 
 #########
-# Step 4: Appropriately label the data set with descriptive variable names
+# Step 4: label the data set with the variable names
 #########
 
 # correct column name
@@ -61,7 +61,7 @@ names(subject_data) <- "subject"
 all_data <- cbind(x_data, y_data, subject_data)
 
 ##########
-# Step 5: Create a second, independent tidy data set with the average of each variable for each activity and each subject
+# Step 5: Create independent tidy data set with the average of each variable for each activity and each subject
 ##########
 
 # 66 <- 68 columns but last two (activity & subject)
